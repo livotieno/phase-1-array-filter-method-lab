05547ce31638553e05b006fcfbaa5d40c50da32d
livotieno <livio5261@gmail.com>
2023-09-26 08:42:32 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function  findMatching(driversName, array){
+    const name = driversName.char(0).toUppercase + driversName.slice(2).toLowerCase();
+    const name2 = driversName.toLowerCase();
+    const filter = array.filter(name);
+    return filter;
+}
+
+function fuzzymatch(driversName ,array){
+    const filterArray = array.filter(value => value.slice(0,3) === driversName)
+    return filter;
+}
+function matchname(driversName, array){
+    const filterArray = array.filter(Name === driversName);
+    return filterArray;
+}
