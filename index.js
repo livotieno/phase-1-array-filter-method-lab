99ebe619bd13fd1142d899bebc83d3c58375695c
livotieno <144663269+livotieno@users.noreply.github.com>
2023-09-28 19:28:03 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-function  findMatching(driversName, array){
-    const name = driversName.charAt(0).toUppercase + driversName.slice(2).toLowerCase();
-    const name2 = driversName.toLowerCase();
-    const filter = array.filter(name);
-    return filter;
-}
+function findMatching(drivers, name){
+    const newDrivers = drivers.filter((driver) => {
+       return driver.toLowerCase() === name.toLowerCase();
+   });
+   if (newDrivers) {
+    return newDrivers;
+   }
+      else{
+        return [];
+      }
 
-function fuzzymatch(driversName ,array){
-    const filterArray = array.filter(value => value.slice(0,3) === driversName)
-    return filter;
-}
-function matchname(driversName, array){
-    const filterArray = array.filter(Name === driversName);
-    return filterArray;
-}
+
+    }
+    
+    function fuzzyMatch(drivers, startingLetters){
+     const newDrivers = drivers.filter((driver) => {
+        return driver.startsWith(startingLetters);
+     });
+
+       return newDrivers;
+
+    }
+
+    function matchName(drivers, name) {
+     const newDrivers = drivers.filter((driver) => {
+        return driver.name === name;
+     });
+      return newDrivers;
+
+    }
